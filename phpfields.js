09f50d11515bc31014921a59b781558081500c76
victortassinari <victortassinari@outlook.com>
2014-10-28 23:09:32 -0200

    Update phpfields.js

--- phpfields.js
+++ phpfields.js
@@ -1,31 +1,11 @@
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1);
-        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
-    }
-    return "";
-}
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-    var codificado = getCookie("fields");
+    var codificado = document.getElementById("fields").value;
     if (codificado != "") {
-
         var decodificado = base64_decode(codificado);
         if (decodificado[decodificado.length - 1] != '}') {
             decodificado = decodificado.substring(0, decodificado.lastIndexOf('}') + 1);
         }
         var x = JSON.parse(decodificado);
-        setCookie("valores", "", -1);
         for (i = 0; i < document.forms.length; i++) {
             for (j = 0; j < document.forms[i].length; j++) {
                 if (document.forms[i][j].type == "text"  || document.forms[i][j].type == "password" || document.forms[i][j].type == "hidden" || document.forms[i][j].type == "select-one") {
@@ -37,8 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
                 } else if (document.forms[i][j].type == "checkbox"){
                     var valor = (eval('x.' + document.forms[i][j].name));
-                    console.log(valor);
-
                     if (valor != undefined && valor == document.forms[i][j].value) {
                         document.forms[i][j].checked = 'checked';
                     } else {
